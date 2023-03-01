a67a606d09077023649c75c3e36b5469ec90a70e
Simone Carcagnì <simonecarcagni@libero.it>
2023-03-01 17:24:50 +0100

    Lista della spesa stampata in HTML

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let groceriesList = ["uova", "latte", "farina", "pane", "zucchero", "fazzoletti"];
+
+let i = 0;
+
+const listaSpesaDom = document.querySelector('.lista-spesa');
+
+while(i < groceriesList.length){
+
+    let listItem = `<li> ${groceriesList[i]}</li>`;
+
+    listaSpesaDom.innerHTML += listItem;
+    i++
+}
+
+console.log(listaSpesaDom);
